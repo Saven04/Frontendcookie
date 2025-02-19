4700e87bcdf3938cc039cc8c4c45246e90c3685e
Savenx20 <22337044@student.hindustanuniv.ac.in>
2025-02-20 00:40:11 +0530

    Update cookie-banner.js

--- public/src/cookie-banner.js
+++ public/src/cookie-banner.js
@@ -154,7 +154,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     function getCookie(name) {
-        const value = ; ${document.cookie};
+        const value = ${document.cookie};
         const parts = value.split(; ${name}=);
         if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
     }
