66835e361884d92c0a954ec0c9c18c40c0d7ecc8
Savenx20 <22337044@student.hindustanuniv.ac.in>
2025-03-07 18:36:25 +0530

    Update register.js

--- public/src/register.js
+++ public/src/register.js
@@ -61,3 +61,14 @@ document.getElementById("registerForm").addEventListener("submit", async functio
         resetButton();
     }
 });
+
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split("=");
+        if (cookieName === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+    return null;
+}
